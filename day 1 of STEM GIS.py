49a6208464d8ba029a24e873ec1f7c152d5e027f
bascussienna <52932137+bascussienna@users.noreply.github.com>
2019-07-15 15:53:28 -0400

    Add files via upload

---
+++ day 1 of STEM GIS.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+3+11 #addition
+print("hello world")
+print("i am learning to code today")
+print("123") 
+print(5*2)
+print(5/2)
+print(5-2)
+print(5**2)
+print(8/9*3)
+print("the division of 5 by 3 is", 5/3)
+def plus(a,b):
+    plus = a + b
+    print(plus)
+
+plus(2,3)
+
+#create function to find the product of two numbers
+def divide(a,b):
+    divide = a / b
+    print(divide)
+    
+divide(50,5)   
+
+def tri(a,b,c):
+    tri = a*b*c
+    print("the area of a triangle with base", b, "and height", c, "is", tri)
+    
+    tri(1/2,2,4)

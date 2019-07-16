c25eb722eef220f63ee13be245bb3cc43f43d3d3
bascussienna <52932137+bascussienna@users.noreply.github.com>
2019-07-16 16:00:48 -0400

    Add files via upload

---
+++ day 2 of STEM GIS.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 10:32:45 2019
+
+@author: admin
+"""
+
+def areasquare(a):
+    areasquare = a**2
+    print(areasquare)
+    
+areasquare(4)
+
+print("box", =5)
+
+box = 5
+print("there are",box, "bars in the box")
+
+#chocolate types
+d = "dark" 
+m = "milk"
+w = "white"
+
+print(d,m,w)
+"d" = dark
+dark = "d"
+
+ a= "sienna"
+print("hi",a, "nice to meet you")
+
+def hi (a):
+    hi = a 
+    print ("hi", a)
+hi("sienna")
+
+import math
+dir(math)
+ 
+math.sqrt(36)
+math.remainder(10/7)
+math.pow(2,4)
+math.pow(2,3)
+
+def cubicroot(a):
+    cubicroot = a**(1/3)
+    print("the cubic root of", a, "is",cubicroot)
+    
+
+cubicroot(27)
+
+def cube(x):
+    cube= math.pow(x,1/3)
+    print("the cube root of", x, "is", cube)
+
+cube(27)
+    
+import math
+print("PLEASE ENTER A VALUE")
+y=20
+
+def cube(y):
+    cube= math.pow(y, 1/3)
+    print("the cube root of", y, "is", cube)
+    
+cube(y)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
